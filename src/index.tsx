20d329ad8b9256a55217bfa856bd38635d6d8ed7
haku1217 <haruki12.17birthday@gmail.com>
2019-04-25 18:33:54 +0900

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
+
+interface ISquareProps{
+  value: number;
+}
+
+interface IState{
+  value: string;
+}
+
+class Square extends React.Component<ISquareProps,IState> {
+  public render() {
+    return (
+      <button className="square">
+        <div>
+          {this.props.value}
+        </div>
+      </button>
+    );
+  }
+}
+
+
+class Board extends React.Component{
+  renderSquare(i: number) {
+    return <Square value={i}/>;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
